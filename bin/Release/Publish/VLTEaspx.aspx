36d7682def9dc3cbe52e03d5c045a3565b148dd0
pp0612 <praveen.p@msn.com>
2018-06-21 15:36:10 +0530

    Initial publishing

---
+++ bin/Release/Publish/VLTEaspx.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ViewLTE.Master" AutoEventWireup="true" CodeBehind="VLTEaspx.aspx.cs" Inherits="ViewLTE_webapp.VLTEaspx" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder" runat="server">
+      <div class="jumbotron">
+          <p class="nav navbar-text navbar-right">Hello, <asp:LoginName runat="server" />!</p>
+        <h1>ASP.NET</h1>
+        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
+        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+</asp:Content>
---
+++ obj/Release/Package/PackageTmp/Assets/includes/Pages-Navigation.js
@@ -0,0 +1,44 @@
+document.write('<!-- Left Panel -->\
+<aside id="left-panel" class="left-panel">\
+    <nav class="navbar navbar-expand-sm navbar-default">\
+        <div class="navbar-header">\
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">\
+                <i class="fa fa-bars"></i>\
+            </button>\
+            <a class="navbar-brand" href="../index.html">ViewLTE</a>\
+            <a class="navbar-brand hidden" href="../index.html">VLT .</a>\
+        </div>\
+        <div id="main-menu" class="main-menu collapse navbar-collapse">\
+            <ul class="nav navbar-nav">\
+                  <li >\
+                      <a href="../index.html"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>\
+                </li>\
+                <li >\
+                    <a href="./VITS-Home.html"> <i class="menu-icon fa fa-bolt"></i>VITS</a>\
+              </li>\
+                <li class="menu-item-has-children dropdown">\
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"> <i class="menu-icon fa fa-file"></i>Reports</a>\
+                    <ul class="sub-menu children dropdown-menu">\
+                        <li class><i class="menu-icon fa fa-file"></i><a href="CDP-Reports.html">CDP Reports</a></li>\
+                        <li><i class="menu-icon fa fa-file"></i><a href="Capacity-Reports.html">Capacity Report</a></li>\
+                    </ul>\
+                </li>\
+            </ul>\
+        </div><!-- /.navbar-collapse -->\
+    </nav>\
+</aside><!-- /#left-panel -->\
+<!-- Left Panel -->\
+<!-- Right Panel -->\
+<div id="right-panel" class="right-panel">\
+    <!-- Header-->\
+    <header id="header" class="header">\
+        <div class="header-menu">\
+            <div class="col-sm-12">\
+                <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>\
+                <div class="header-left">\
+                </div>\
+            </div>\
+        </div>\
+    </header><!-- /header -->\
+    <!-- Header-->\
+  ');
